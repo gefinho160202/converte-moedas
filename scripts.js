ce6411985ea0dd0263d01beda1c84568f4d7333b
gefinho160202 <106202169+gefinho160202@users.noreply.github.com>
2022-11-08 21:16:50 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
+const dolar = 5.18
+const euro = 5.12
+const bitcoin = 106465.44
+
+
+const convertValues = () => {
+    const inputReal = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReal)
+
+    if (select.value === "U$$ Dolar americano") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReal / dolar)
+    }
+
+    if (select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReal / euro)
+    }
+    if (select.value === "₿ Bitcoin") {
+        currencyValueText.innerHTML = ((inputReal / bitcoin).toFixed(6))
+    }
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+
+    if (select.value === "U$$ Dolar americano") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImg.src = "./assets/estados-unidos (1) 1.png"
+    }
+
+    if (select.value === "€ Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/Design sem nome 1.png"
+    }
+
+    if (select.value === "₿ Bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin.png"
+    }
+    convertValues()
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
+
